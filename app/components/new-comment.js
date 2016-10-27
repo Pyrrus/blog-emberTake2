c1dfb1b89e4137069ccb3a5c3ebd06640e6797fe
Adam Gorbahn and Stephen Emery <stephen@>
2016-10-27 14:32:04 -0700

    Fix error.

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -4,9 +4,9 @@ export default Ember.Component.extend({
   actions: {
     saveComment(blog) {
      var params = {
-       author: this.get('author'),
-       rating: this.get('rating'),
-       content: this.get('content'),
+       author: this.get('author') ? this.get('author') : "You for got something!!",
+       rating: this.get('rating') ? this.get('rating') : 1,
+       content: this.get('content') ? this.get('content') : "You for got something!!",
        blog: blog
      };
      this.sendAction('saveComment', params, this.get('id'));
