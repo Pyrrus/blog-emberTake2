760e197abbc7d5cf5b17a0a1cc0f337771093712
Adam Gorbahn and Stephen Emery <adam@>
2016-10-27 09:04:39 -0700

    Add delete functionality.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('blog');
   },
+  actions: {
+    deleteBlog(blog) {
+      if(confirm("Are you sure you want to delete this blog?")) {
+        blog.destroyRecord();
+        this.transitionTo('admin');
+      }
+    }
+  }
 });
